464bda7c6c52c87ae8144b61e67296b7122655c1
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-21 08:59:30 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -19,15 +19,15 @@ async def refunc(client,message):
         		await message.reply_to_message.delete()
         		if mime == "video":
         			markup = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("📁 Document",callback_data = "doc"), 
-        			InlineKeyboardButton("🎥 Video",callback_data = "vid") ]])
+        			InlineKeyboardButton("Document",callback_data = "doc"), 
+        			InlineKeyboardButton("Video",callback_data = "vid") ]])
         		elif mime == "audio":
-        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc")
-        			,InlineKeyboardButton("🎵 audio",callback_data = "aud") ]])
+        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Document",callback_data = "doc")
+        			,InlineKeyboardButton("Audio",callback_data = "aud") ]])
         		else:
-        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc") ]])
+        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Document",callback_data = "doc") ]])
         		# dont chenge this message.reply_text     			        		
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",reply_to_message_id=mg_id,reply_markup = markup)
+        		await message.reply_text(f"**Select OutPut Media Type :**\n**New FileName** :- ```{out_filename}```",reply_to_message_id=mg_id,reply_markup = markup)
         		
         	except:
         		try:
@@ -36,19 +36,19 @@ async def refunc(client,message):
         			out_filename= new_name + "."+ out_name
         		except:
         			await message.reply_to_message.delete()
-        			await message.reply_text("**Error** :  No  Extension in File, Not Supporting"
+        			await message.reply_text("**Error** :  No  Extension In File, Add OutPut Extension. Example : .mkv,.mp4, .m4a"
         			,reply_to_message_id=mg_id)
         			return
         		await message.reply_to_message.delete()
         		if mime == "video":
-        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc")
-        			,InlineKeyboardButton("🎥 Video",callback_data = "vid") ]])
+        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Document",callback_data = "doc")
+        			,InlineKeyboardButton("Video",callback_data = "vid") ]])
         		elif mime == "audio":
-        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc")
-        			,InlineKeyboardButton("🎵 audio",callback_data = "aud") ]])
+        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Document",callback_data = "doc")
+        			,InlineKeyboardButton("Audio",callback_data = "aud") ]])
         		else:
-        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc") ]])
+        			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Document",callback_data = "doc") ]])
         		# dont chenge this message.reply_text 
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",
+        		await message.reply_text(f"**Select OutPut Media Type :**\n**New FileName** :- ```{out_filename}```",
         		reply_to_message_id=mg_id,reply_markup = markup)
         		
